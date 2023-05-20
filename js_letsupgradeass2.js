b2acd7334fcbecef093e9553ac4ecb6a93cf3b2a
Navadhesh <133025163+Navadhesh@users.noreply.github.com>
2023-05-20 19:13:35 +0530

    completed letsupgarde assignment day3

---
+++ js_letsupgradeass2.js
@@ -0,0 +1,7 @@
+var fruits=["apple","banana","orange","grape","mango"];
+console.log("The array length is:",fruits.length);
+console.log("The array elements are:");
+for(var i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i]);
+}
